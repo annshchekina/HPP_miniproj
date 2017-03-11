c94cab6679b74c7e0364281718317004f8f60521
annshchekina <annshchekina@gmail.com>
2017-03-11 02:28:44 +0100

    AVX

--- funcs.c
+++ funcs.c
@@ -4,13 +4,18 @@
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
+#include <immintrin.h>
 
 #define characteristic_coeff 3
+#define VECT_LEN 4
 
 double sqrt_Func1, pow_Func1;
 const double inv_Func2 = 1 / 300, inv_Func3 = 1 / 3;
 
-double Func1(unsigned char a, double * time_sec) { // 1B is a char
+__m256d a_Func1, b_Func1, c_Func1, d_Func1, e_Func1; 
+
+double Func1(unsigned char a, double * time_sec) 
+{ 
   double start = get_wall_seconds();
   double y = sqrtf(a) * sqrt_Func1 + 0.12;
   double z = powf(a, 1.3) * pow_Func1;
@@ -20,7 +25,22 @@ double Func1(unsigned char a, double * time_sec) { // 1B is a char
   return x;
 }
 
-double Func2(unsigned char a, unsigned char b, double * time_sec, double * time_sec_Func1) {
+__m256d Func1_avx(const __m256d v1, const __m256d v2, 
+  double * time_sec) 
+{  
+  double start = get_wall_seconds();
+  __m256d x = _mm256_mul_pd(v2, c_Func1);
+  __m256d y = _mm256_add_pd(_mm256_mul_pd(v1, a_Func1), b_Func1);
+  __m256d z = _mm256_div_pd(x, y);
+  __m256d w = _mm256_add_pd(z, d_Func1);
+  __m256d r = _mm256_mul_pd(_mm256_mul_pd(w, w), e_Func1);
+  *time_sec += get_wall_seconds() - start;
+  return r;
+}
+
+double Func2(unsigned char a, unsigned char b, 
+  double * time_sec, double * time_sec_Func1) 
+{
   double start = get_wall_seconds();
   int aa = a;
   int bb = b;
@@ -38,7 +58,8 @@ double Func2(unsigned char a, unsigned char b, double * time_sec, double * time_
 }
 
 // This function expects the input array a to consist of 4 numbers.
-double Func3(unsigned char* a, double p, double * time_sec) {
+double Func3(unsigned char* a, double p, double * time_sec) 
+{
   double start = get_wall_seconds();
   double* v = malloc(4*sizeof(double));
   double* w = malloc(4*sizeof(double));
@@ -63,17 +84,64 @@ double Func3(unsigned char* a, double p, double * time_sec) {
 
 double ComputeNumber(unsigned char* buf, int nBytes, double p, 
 	double * Func1_time, double * Func1_Func2_time, 
-	double * Func2_time, double * Func3_time) {
+	double * Func2_time, double * Func3_time) 
+{
   assert(p == characteristic_coeff);
+  
   sqrt_Func1 = sqrt(0.001);
   pow_Func1 = pow(0.001, 1.3);
-  int i;
-  double sum = 0;
+  
+  a_Func1 = _mm256_set1_pd(sqrt_Func1);
+  b_Func1 = _mm256_set1_pd(0.12);
+  c_Func1 = _mm256_set1_pd(pow_Func1);
+  d_Func1 = _mm256_set1_pd(0.4);
+  e_Func1 = _mm256_set1_pd(0.04);
+  
+  int i, j;
+  double * v1_Func1 = (double *)malloc(nBytes * sizeof(double)), 
+  	* v2_Func1 = (double *)malloc(nBytes * sizeof(double));
+  
+  double start = get_wall_seconds();
   for(i = 0; i < nBytes; i++)
-    sum += Func1(buf[i], Func1_time);
-  for(i = 0; i < nBytes-1; i+=2)
+  {
+	  
+	  v1_Func1[i] = sqrtf(buf[i]);
+	  v2_Func1[i] = powf(buf[i], 1.3);
+  }
+  *Func1_time += get_wall_seconds() - start;
+  
+  double sum = 0;
+ 
+  int n = nBytes - nBytes % VECT_LEN;
+  for(i = 0; i < n; i += VECT_LEN)
+  { 
+	__m256d v1 = _mm256_loadu_pd(v1_Func1 + i); 
+	__m256d v2 = _mm256_loadu_pd(v2_Func1 + i); 
+	__m256d v = Func1_avx(v1, v2, Func1_time);
+	
+	double res_vec[VECT_LEN];
+	_mm256_storeu_pd(res_vec, v);
+	for(j = 0; j < VECT_LEN; j++)
+		sum += res_vec[j];
+  }
+  for(i = n; i < nBytes; i++)
+  {
+	  sum += Func1(buf[i], Func1_time);
+  }
+  
+  /*for(i = 0; i < nBytes; i++)
+  {
+  	sum += Func1(buf[i], Func1_time);
+  }*/
+  
+  for(i = 0; i < nBytes - 1; i += 2)
     sum += Func2(buf[i], buf[i+1], Func2_time, Func1_Func2_time);
-  for(i = 0; i < nBytes-3; i+=4)
+  
+  for(i = 0; i < nBytes - 3; i += 4)
     sum += Func3(&buf[i], p, Func3_time);
+    
+  free(v1_Func1);
+  free(v2_Func1);
+  
   return sum;
 }
